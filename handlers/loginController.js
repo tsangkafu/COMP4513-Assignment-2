1b4e454c7cd625f63b304fbdb4a671a43a506e04
tsangkafu <tsangkafu@ymail.com>
2023-04-02 16:36:44 -0600

    finished authentication

---
+++ handlers/loginController.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcrypt");
+
+// if the user exists, return the password_bcrypt
+const getBcrypt = (User, username) => {
+    return new Promise ((res, rej) => {
+        User.find({email: username})
+            // return the bcrypt if user exists else return null
+            .then((data) => data.length > 0 ? res(data[0].password_bcrypt) : res(null))
+            .catch((err) => rej(err));
+    })
+}
+
+// returning true or false indicating if the password matches
+const authenticate = (password, password_bcrypt) => {
+    // if user exists, compare the digest
+    return new Promise ((res, rej) => {
+        bcrypt.compare(password, password_bcrypt, function(err, result) {
+            err? rej(err) : res(result);
+        })
+    })
+}
+
+module.exports = {
+    getBcrypt,
+    authenticate
+};
--- handlers/loginRouter.js
+++ handlers/loginRouter.js
@@ -1,19 +1,32 @@
+const controller = require('./loginController.js');
+
 const login = (app, User) => {
     // if not login
     app.get('/', (req, res) => {
         res.render('login');
     });
 
-    console.log(User.find());
-
+    // if login successfuly
     app.post('/login', (req, res) => {
         const { username, password } = req.body;
-        // Check if username and password are correct
-        if (username === 'myusername' && password === 'mypassword') {
-            res.send('Logged in successfully!');
-        } else {
-            res.send('Incorrect username or password.');
-        }
+        // if the user exists
+        controller.getBcrypt(User, username)
+            .then((password_bcrypt) => {
+                if (password_bcrypt !== null) {
+                    // User exists, check if password match
+                    controller.authenticate(password, password_bcrypt)
+                        .then((result) => {
+                            if (result) {
+                                res.render("login", { error: "Correct password." });
+                            } else {
+                                res.render("login", { error: "Incorrect password." });
+                            }
+                        })
+                } else {
+                    // User does not exist
+                    res.render("login", { error: "User does not exists." });
+                }
+            })
     });
 }
 
